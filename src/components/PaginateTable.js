53c57d562313f65bd0d301b3d76c483c37bd396b
冯捷 <fengjie@s-MacBook-puro.local>
2020-08-31 14:45:26 -0400

    scroll table

--- src/components/PaginateTable.js
+++ src/components/PaginateTable.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-import Media from 'react-media';
 import {
     Card,
     CardHeader,
@@ -11,14 +10,13 @@ import {
     Button,
 } from "reactstrap";
 
-import { useSelector, useDispatch } from 'react-redux';
-import { selectAllData, updateByKey } from '../reducer';
+import { useSelector } from 'react-redux';
+import { selectAllData } from '../reducer';
 
 export function PaginateTable(props) {
 
     const [startIdx, setStartIdx] = useState(0);
     const data = useSelector(selectAllData);
-    const dispatch = useDispatch();
 
     let paginateBackward = () => {
       if(startIdx - 20 >= 0) {
@@ -32,12 +30,12 @@ export function PaginateTable(props) {
       }
     }
   
-    let thead = ["日期", "累计检测", "新增检测", "检测未出结果", "累计确诊", "治愈", "住院", "死亡", "今日新增", "现存确诊"];
+    let thead = ["日期", "总检测", "新增检测", "待定结果", "总确诊", "治愈", "住院", "死亡", "当日新增", "现存确诊"];
     let tbody = data.map((prop, key) => {
         return (
           <tr key={key}>
             <td className="text-left">
-              {prop.date.toString().substring(0, 10)}
+              {prop.date.toString().substring(5, 10)}
             </td>
             <td className="text-left">
               {prop.total_test}
@@ -73,31 +71,6 @@ export function PaginateTable(props) {
           </tr>
         );
       })
-
-    let thead_sm = ["日期", "累计确诊", "今日新增", "新增检测"];
-    let tbody_sm = []
-    for (let key in data) {
-        tbody_sm.push(
-            <tr key={key} onClick={() => dispatch(updateByKey(key))}>
-                <td className="text-left">
-                {data[key].date.toString().substring(5, 10)}
-                </td>
-                <td className="text-left">
-                {data[key].positive}
-                </td>
-                <td className="text-left">
-                {data[key].new_positive}
-                </td>
-                <td className="text-left">
-                {
-                    (data[key].new_test === undefined)
-                    ? "无数据"
-                    : data[key].new_test
-                }
-                </td>
-            </tr>
-        )
-    }
         
     return (
         <Card>
@@ -107,37 +80,18 @@ export function PaginateTable(props) {
             </CardHeader>
 
             <CardBody>
-                <Media query={{ maxWidth: 799 }}>
-                    {matches =>
-                        matches ? (
-                        <Table responsive>
-                            <thead className="text-primary">
-                            <tr>
-                                {thead_sm.map((prop, key) => {
-                                return <th key={key}>{prop}</th>;
-                                })}
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {tbody_sm.reverse().slice(startIdx, startIdx+20)}
-                            </tbody>
-                        </Table>
-                        ) : (
-                        <Table responsive>
-                            <thead className="text-primary">
-                            <tr>
-                                {thead.map((prop, key) => {
-                                return <th key={key}>{prop}</th>;
-                                })}
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {tbody.reverse().slice(startIdx, startIdx+20)}
-                            </tbody>
-                        </Table>
-                        )
-                    }
-                </Media>
+              <Table responsive style={{minWidth: "1000px"}}>
+                <thead className="text-primary">
+                  <tr>
+                      {thead.map((prop, key) => {
+                      return <th key={key}>{prop}</th>;
+                      })}
+                  </tr>
+                </thead>
+                <tbody>
+                  {tbody.reverse().slice(startIdx, startIdx+20)}
+                </tbody>
+              </Table>
             </CardBody>
 
             <CardFooter className="text-center">
